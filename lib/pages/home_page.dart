6051e466c5f18929024ddc86f429d770b29b7358
KeingCasabona <keing.limache.casabona@gmail.com>
2024-10-06 18:45:30 -0500

    feat: Add MovieMode

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_codigo_movieapp/ui/widget/item_movie_widget.dart';
+import 'package:flutter_codigo_movieapp/ui/widgets/item_movie_widget.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
